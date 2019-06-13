ad937b869f47712d7f38d7dab32b369fa0bae35d
Esdras Xavier <esdrasxa@gmail.com>
2019-06-13 19:13:46 -0300

    Add bluetooth

---
+++ app/src/main/java/br/org/catolicasc/trekking/BluetoothView.java
@@ -0,0 +1,13 @@
+package br.org.catolicasc.trekking;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BluetoothView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bluetooth_view);
+    }
+}
